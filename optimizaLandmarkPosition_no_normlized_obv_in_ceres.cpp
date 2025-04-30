bd62edc22c94f086bac608bd13696d7d49c477b7
JinXiangLai <3292795816@qq.com>
2025-04-30 20:40:08 +0800

    1. 添加对于退化运动下,非退化像素与退化像素所构建系数矩阵的关系 2. 补充非退化运动+退化像素的系数矩阵结果

--- optimizaLandmarkPosition_no_normlized_obv_in_ceres.cpp
+++ optimizaLandmarkPosition_no_normlized_obv_in_ceres.cpp
@@ -191,7 +191,7 @@ int main(int argc, char** argv) {
         const double moveDist = md(gen2);
         // 生成旋转及平移方向
         Eigen::Vector3d rotDir(0, 0, md(gen1));  // 主要绕Z轴，不然就翻车了
-        Eigen::Vector3d posDir(0.00, 0.00,
+        Eigen::Vector3d posDir(md(gen2), md(gen2),
                                md(gen2));  // 沿任何轴平移均可
 
         // 产生下一个位姿
